f736ff73a062cc5ad99b8528d148891671d511f1
Adolfo Zepeda <adolfitozepeda@gmail.com>
2024-05-10 15:03:21 -0600

    footer commit

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,12 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slides = document.querySelectorAll('.slide');
-    let currentSlide = 0;
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slides img');
+const totalSlides = slides.length;
 
-    function nextSlide() {
-        slides[currentSlide].classList.remove('active');
-        currentSlide = (currentSlide + 1) % slides.length;
-        slides[currentSlide].classList.add('active');
+function showSlide(index) {
+    if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else if (index >= totalSlides) {
+        currentSlide = 0;
     }
 
-    setInterval(nextSlide, 3000);
-});
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+
+    slides[currentSlide].style.display = 'block';
+}
+
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+}
+
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
